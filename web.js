fe34f7de75f8edd433f38ff3b6ee5d5feda0e99e
cacophobe <cacophobe@mail.com>
2013-07-15 14:13:48 +0000

    read index.html from file

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2u2!');
+  response.send(buf.toString(fs.readFileSync(index.html)));
 });
 
 var port = process.env.PORT || 5000;
