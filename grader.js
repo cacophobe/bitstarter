3dd0a81b8e06e1cde71ee7c3ee895da58dea88ec
sans comic <sans_comic@yahoo.com>
2013-07-27 23:45:29 +0530

    clean up grader.js

--- grader.js
+++ grader.js
@@ -5,15 +5,6 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var restler = require('restler');
 
-var assertFileExists = function(infile) {
-	var instr = infile.toString();
-	if(!fs.existsSync(instr)) {
-		console.log("%s does not exist. Exiting.", instr);
-		process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-	}
-	return instr;
-};
-
 var cheerioHtmlFile = function(htmlfile) {
 	return cheerio.load(fs.readFileSync(htmlfile));
 };
@@ -33,12 +24,6 @@ var checkHtml = function(htmldata, checksfile) {
 		out[checks[ii]] = present;
 	}
 	return out;
-};
-
-var clone = function(fn) {
-	// Workaround for commander.js issue.
-	// http://stackoverflow.com/a/6772648
-	return fn.bind({});
 };
 
 if(require.main == module) {
